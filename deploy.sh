bfc67d94b7d0c8d7b1771f55087d8bc47a3dea68
Picker666 <1034790395@qq.com>
2021-12-09 10:33:59 +0800

    change deploy config file

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:picker666/picker666.github.io.git master
+# git push -f git@github.com:picker666/picker666.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:picker666/blog.git master:gh-pages
+git push git@github.com:Picker666/blog.git master:deploy
 
 cd -
