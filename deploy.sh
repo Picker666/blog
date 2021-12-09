f66574fce2666087201250933ab0b995727f7ecc
Picker666 <1034790395@qq.com>
2021-12-09 13:33:37 +0800

    remove test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:picker666/picker666.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push git@github.com:Picker666/blog.git master:deploy
+git push -f git@github.com:Picker666/blog.git master:deploy
 
 cd -
