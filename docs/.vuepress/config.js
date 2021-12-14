ca0cb14f906ad126721f6db6ea6d6e47b341d850
Picker666 <1034790395@qq.com>
2021-12-14 10:14:41 +0800

    change fontEndBase to base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     smoothScroll: true,
     lastUpdated: '更新时间',
     nav: [
-      { text: '基础梳理', link: '/fontEndBase/' },
+      { text: '基础梳理', link: '/base/' },
       { text: '手写', link: '/newFunction/' },
       { text: '源码分析', link: '/sourceAnalysis/' },
       { text: 'React', link: '/react/' },
@@ -25,7 +25,7 @@ module.exports = {
       { text: 'Github', link: 'https://github.com/Picker666/blog' },
     ],
     sidebar: {
-      '/fontEndBase/': [
+      '/base/': [
         '',
         'copy',
         'toString'
