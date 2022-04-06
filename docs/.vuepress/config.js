fcef97dc3ac8a6d3855a422af123f269aae87a5a
Picker666 <1034790395@qq.com>
2022-04-06 20:53:22 +0800

    note UDP

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
         "Proxy",
         "Reflect"
       ],
-      "/base/browser/": ["", "storage", "crossDomain", "browserCache", "httpCode"],
+      "/base/browser/": ["", "storage", "crossDomain", "browserCache", "httpCode", "UDP"],
       "/base/security/": ["", 'xss', "CSRF", "SQLInjection", "cookieHold", "clickJacking"],
       "/base/interview/": ["", "scope", "arrayObject"],
       "/typescript/": [
