5d4fdf757018e5c16c3d8f6d4a7466f812b1ea84
Picker666 <1034790395@qq.com>
2023-03-31 21:25:09 +0800

    change bash

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 移除dist
-#  rm -rf docs/.vuepress/dist
+ rm -rf docs/.vuepress/dist
 
 # 生成静态文件
 npm run build
@@ -15,9 +15,9 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git init
+git init
 git add -A
-git commit -m 'deploy' + date 
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:picker666/picker666.github.io.git master
