a8b2e012a1e496c06547540a197ff29be0fcb2b4
Christine-Only <tsy17321289030@163.com>
2023-06-04 16:42:26 +0800

    next image.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -205,7 +205,8 @@ module.exports = {
         "loadableComponent",
         "componentControlled",
         "react18",
-        "nextRenderWay"
+        "nextRenderWay",
+        "nextImages"
       ],
       "/git/": [
         "",
